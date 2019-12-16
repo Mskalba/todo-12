aab131c0fb3eca28f7b27e54ab16286bfdf0c2ab
mskalba <mskalba@eworklabs.io>
2019-12-16 18:40:37 +0100

    Async

--- script.js
+++ script.js
@@ -1,36 +1,62 @@
-let $addBtn, $myInput;
+
+
+
+let $approveBtn, $editInput, $modal, $list;
 
 function main() {
-    const $list = document.getElementById("list");
-    $addBtn = document.getElementById("addTodo");
-    $myInput = document.getElementById("myInput");
+    prepareDOMElements();
+    prepareDOMEvents();
+    getTodos();
+}
+
+function prepareDOMElements() {
+    $approveBtn = document.getElementById("editTodo");
+    $editInput = document.getElementById("popupInput");
+    $modal = document.getElementById("modal");
+    $approveBtn.addEventListener("click", approveBtnClickHandler);
+    $list = document.getElementById("list");
+}
+
+function prepareDOMEvents() {
     $list.addEventListener("click", listClickHandler);
-    $addBtn.addEventListener("click", addBtnClickHandler);
+
+    const $cancelButton = document.getElementById("cancelChanges");
+    const $closeButton = document.getElementById("closePopup");
+    $cancelButton.addEventListener("click", closePopup);
+    $closeButton.addEventListener("click", closePopup);
+}
+
+function getTodos() {
+    axios.get("http://195.181.210.249:3000/todo/")
+        .then(res => { console.log(res.data) })
+        .catch(err => { console.error(err) });
 }
 
 function listClickHandler(event) {
     if (event.target.tagName === "BUTTON") {
         const [action, id] = event.target.dataset.id.split("-");
         if(action === "edit") {
-            $myInput.value = event.target.parentElement.firstChild.textContent.trim();
-            $addBtn.innerText = "Edit " + id;
-            $addBtn.dataset.action = "edit";
-            $addBtn.dataset.editId = id;
+            $editInput.value = event.target.parentElement.firstChild.textContent.trim();
+            $approveBtn.dataset.editId = id;
+            openPopup();
         }
     }
 }
 
-function addBtnClickHandler(event) {
-    if (event.target.dataset.action === "edit") {
-        const editedElement = document.getElementById("todo-" + event.target.dataset.editId);
-        editedElement.firstChild.textContent = $myInput.value;
-        $addBtn.innerText = "Add";
-        $addBtn.dataset.action = "add";
-        $addBtn.dataset.editId = undefined;
-        $myInput.value = "";
-    } else {
-        // TODO
-    }
+function approveBtnClickHandler(event) {
+    const editedElement = document.getElementById("todo-" + event.target.dataset.editId);
+    editedElement.firstChild.textContent = $editInput.value;
+    $approveBtn.dataset.editId = undefined;
+    $editInput.value = "";
+    closePopup();
+}
+
+function openPopup() {
+    $modal.style.display = "block";
+}
+
+function closePopup() {
+    $modal.style.display = "none";
 }
 
 document.addEventListener("DOMContentLoaded", main);

1450819b522731e75c79c7aa4948f3c010f12293
mskalba <mskalba@eworklabs.io>
2019-12-18 18:01:39 +0100

    async await edition with server

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 
 let $approveBtn, $editInput, $modal, $list;
-
+const BASE_URL = "http://195.181.210.249:3000/todo/";
 function main() {
     prepareDOMElements();
     prepareDOMEvents();
@@ -26,10 +26,54 @@ function prepareDOMEvents() {
     $closeButton.addEventListener("click", closePopup);
 }
 
-function getTodos() {
-    axios.get("http://195.181.210.249:3000/todo/")
-        .then(res => { console.log(res.data) })
-        .catch(err => { console.error(err) });
+function addElementToList(item) {
+    const newLi = document.createElement("li");
+    newLi.classList.add("todo");
+    newLi.id = "todo-" + item.id;
+    newLi.innerText = item.title;
+
+    const editBtn = document.createElement("button");
+    editBtn.dataset.id= "edit-" + item.id;
+    editBtn.style.float= "right";
+    editBtn.innerText = "Edit";
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.dataset.id= "delete-" + item.id;
+    deleteBtn.style.float= "right";
+    deleteBtn.innerText = "Delete";
+
+    newLi.appendChild(editBtn);
+    newLi.appendChild(deleteBtn);
+
+    $list.appendChild(newLi);
+}
+
+async function getTodos() {
+    try {
+        const response = await axios.get(BASE_URL);
+        response.data.forEach(addElementToList)
+    } catch(err) {
+        console.log("Blad z serwera");
+    }
+
+
+
+    // axios.get("http://195.181.210.249:3000/todos/")
+    //     .then(res => { console.log(res.data) })
+    //     .catch(err => { console.error("jest blad") });
+
+    // fetch("http://195.181.210.249:3000/todo/", {method: "POST"})
+    //     .then(res => res.json())
+    //     .then(data => {console.log(data)})
+    //     .catch(err => { console.error(err) });
+    //
+    // axios.get("http://195.181.210.249:3000/todo/")
+    //     .then(res => res.data)
+    //     .then(data => {
+    //         data.forEach(
+    //             el => { axios.delete("http://195.181.210.249:3000/todo/" + el.id)}
+    //         )
+    //     })
 }
 
 function listClickHandler(event) {
@@ -43,9 +87,15 @@ function listClickHandler(event) {
     }
 }
 
-function approveBtnClickHandler(event) {
-    const editedElement = document.getElementById("todo-" + event.target.dataset.editId);
-    editedElement.firstChild.textContent = $editInput.value;
+async function approveBtnClickHandler(event) {
+    try {
+        await axios.put(BASE_URL + event.target.dataset.editId, { title: $editInput.value });
+        $list.innerHTML = "";
+        await getTodos();
+    } catch(err) {
+        console.log(err)
+    }
+
     $approveBtn.dataset.editId = undefined;
     $editInput.value = "";
     closePopup();
